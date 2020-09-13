0e233798e881d75c841a46544d5b44d8eff8c82a
Iznitek <59332579+Iznitek@users.noreply.github.com>
2020-09-13 15:23:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Привет")
+input()
+print("Как тебя зовут?")
+Name = input()
+print("Класное имя,")
+print(Name)
+print("Как зовут твоего кота?")
+Cat = input()
+print(Cat)
+print("Интересное имя для кота")
+color = "Оранжевый"
+print("Мой любимый цвет -" + color +"Он класный")
+print("Мой любимый цвет %s Он класный." % color)
+
+animal = "кот"
+type = "шотландский"
+print("Мой любимый вид животных - %s, а именно: %s." %(animal, type))
