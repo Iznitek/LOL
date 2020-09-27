fff00916ed480c87858a6ce836b96a02c6739ce3
Iznitek <59332579+Iznitek@users.noreply.github.com>
2020-09-27 15:00:32 +0300

    Update Inted.py

--- Inted.py
+++ Inted.py
@@ -20,40 +20,40 @@ Date = Recived_number // 100
 Month_number = Recived_number - 100*Date
 if Month_number == 1:
     Month_number = "Января"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 2:
     Month_number = "Февраля"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 3:
     Month_number = "Марта"
-    print("Твой день рождения: %s" % Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 4:
     Month_number = "Апреля"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 5:
     Month_number = "Мая"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 6:
     Month_number = "Июня"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 7:
     Month_number = "Июля"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 8:
     Month_number = "Августа"
-    print("Твой день рождения: %s" % Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 9:
     Month_number = "Сентября"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 10:
     Month_number = "Октября"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 11:
     Month_number = "Ноября"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 elif Month_number == 12:
     Month_number = "Декабря"
-    print("Твой день рождения: %s" %Date + Month_number)
+    print("Твой день рождения: %s %s" (Date, Month_number))
 else:
 
         print("Что то пошло не так!")
