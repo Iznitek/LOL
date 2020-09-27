36dd3aac734e718d8791324ade856f97e696cfba
Iznitek <59332579+Iznitek@users.noreply.github.com>
2020-09-27 15:10:17 +0300

    Update Inted.py

--- Inted.py
+++ Inted.py
@@ -1,60 +1,54 @@
 print("Привет")
-print("Сколько тебе лет?")
-a = int(input())
-if a <= 18:
-    print("Ты ещё маленький!")
-elif a >= 18:
-    print("Хочешь отгадаю дату твоего рождения?")
-b = input()
-if b == "да":
-    print("1. Умнож на 2 число дня твоего рождения")
-    print("2. До получившегося числа добавь 5")
-    print("3. Умнож теперь это число на 50")
-    print("4. Добавь номер месяца твоего рождения")
-elif b != "да":
-    print("Хорошо, до новых встреч!")
-
-Recived_number = int(input())
-Recived_number -= 250
-Date = Recived_number // 100
-Month_number = Recived_number - 100*Date
-if Month_number == 1:
-    Month_number = "Января"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 2:
-    Month_number = "Февраля"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 3:
-    Month_number = "Марта"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 4:
-    Month_number = "Апреля"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 5:
-    Month_number = "Мая"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 6:
-    Month_number = "Июня"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 7:
-    Month_number = "Июля"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 8:
-    Month_number = "Августа"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 9:
-    Month_number = "Сентября"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 10:
-    Month_number = "Октября"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 11:
-    Month_number = "Ноября"
-    print("Твой день рождения: %s %s" (Date, Month_number))
-elif Month_number == 12:
-    Month_number = "Декабря"
-    print("Твой день рождения: %s %s" (Date, Month_number))
+print("Хочешь я у гадаю когда у тебя день рождения?")
+choise = input()
+if choise == "Да":
+    print("Введи число от 1 до 12")
+    age = input()
+if age >= "12":
+    print("1. Умнож на 2 чмсло дня своего рождения")
+    print("2. Теперь добавь 5")
+    print("3. Умнож на 50")
+    print("4. Добавь номер месяца своего рождения")
+    print("Какое число ты получил?")
+number = int(input())
+number -= 250
+date = number // 100
+month = number - 100*date
+if month == 1:
+    month = "Января"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 2:
+    month = "Февраля"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 3:
+    month = "Марта"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 4:
+    month = "Апреля"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 5:
+    month = "Мая"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 6:
+    month = "Июня"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 7:
+    month = "Июля"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 8:
+    month = "Августа"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 9:
+    month = "Сентября"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 10:
+    month = "Октября"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 11:
+    month = "Ноября"
+    print("Твоё день рождения: %s %s" %(date, month))
+elif month == 12:
+    month = "Декабря"
+    print("Твоё день рождения: %s %s" %(date, month))
 else:
-
-        print("Что то пошло не так!")
-
+    print("Что то пошло не так!")
